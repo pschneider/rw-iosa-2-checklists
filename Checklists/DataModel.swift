291167c44cb917ead709004123a87a43acf31545
Patrick Schneider <me@patrickschneider.io>
2015-10-23 22:44:35 +0200

    Handle first time app run

--- Checklists/DataModel.swift
+++ Checklists/DataModel.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-struct DataModel {
+class DataModel {
     var lists = [Checklist]()
 
     var indexOfSelectedChecklist: Int {
@@ -21,10 +21,23 @@ struct DataModel {
         }
     }
 
+    func handleFirstTime() {
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        let firstTime = userDefaults.boolForKey("FirstTime")
+        if firstTime {
+            let checklist = Checklist(name: "List")
+            lists.append(checklist)
+            indexOfSelectedChecklist = 0
+            userDefaults.setBool(false, forKey: "FirstTime")
+            userDefaults.synchronize()
+        }
+    }
+
     // MARK: Life Cycle
     init() {
         loadChecklists()
         registerDefaults()
+        handleFirstTime()
     }
 
     // MARK: Persistens
@@ -46,7 +59,7 @@ struct DataModel {
         data.writeToFile(dataFilePath(), atomically: true)
     }
 
-    mutating func loadChecklists() {
+    func loadChecklists() {
         let path = dataFilePath()
         print(path)
         if NSFileManager.defaultManager().fileExistsAtPath(path),
@@ -59,7 +72,10 @@ struct DataModel {
 
     // MARK: NSUserdefaults defaults
     func registerDefaults() {
-        let dictionary = ["ChecklistIndex": -1]
+        let dictionary = [
+            "ChecklistIndex": -1,
+            "FirstTime": true
+        ]
         NSUserDefaults.standardUserDefaults().registerDefaults(dictionary)
     }
 }
