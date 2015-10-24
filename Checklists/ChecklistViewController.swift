a5646a8386e20f6093b7ba5fa76461adcb44de23
Patrick Schneider <me@patrickschneider.io>
2015-10-24 16:07:32 +0200

    App Icons, Constraints for text fields

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -76,6 +76,7 @@ class ChecklistViewController: UITableViewController, ItemDetailViewControllerDe
     func configureCheckmarkForCell(cell: UITableViewCell, withChecklistItem item: ChecklistItem) {
         let label = cell.viewWithTag(1001) as! UILabel
         label.text = item.checked ? "√" : ""
+        label.textColor = view.tintColor
     }
 
     func configureTextForCell(cell: UITableViewCell, withChecklistItem item: ChecklistItem) {
